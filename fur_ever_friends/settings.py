63e9c8a4d94e97597dcad88b7f0324abd51ee95b
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-05-07 08:54:56 +0600

    db configuration

--- fur_ever_friends/settings.py
+++ fur_ever_friends/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mom=u+b__^ayls-uoy!8k@m#kpyj48*2zf@bpt9wxm24jjar(n'
-
+# SECRET_KEY = ''
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://fur-ever-friends-backend.onrender.com','https://*.127.0.0.1']
+
 CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 CSRF_TRUSTED_ORIGINS = [
@@ -91,10 +91,21 @@ WSGI_APPLICATION = 'fur_ever_friends.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
