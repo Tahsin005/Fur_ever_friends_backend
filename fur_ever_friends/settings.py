21c43ba0e4431a109a981602949a025d7a59918b
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-05-07 12:58:58 +0600

    -

--- fur_ever_friends/settings.py
+++ fur_ever_friends/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['https://fur-ever-friends-backend.onrender.com','https://*.127.0.0.1']
 # Application definition
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5500",
