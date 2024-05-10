a1e9998bb481548634a95cdfbcca6929f49a8f74
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-05-10 10:08:52 +0600

    changes in model

--- account/models.py
+++ account/models.py
@@ -6,7 +6,7 @@ class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
     account_no = models.IntegerField(unique=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    image = models.ImageField(upload_to="images/user")
+    image = models.ImageField(upload_to="account/uploads/images/")
     
     def __str__(self) -> str:
         return f'{self.user.username} : {self.account_no}'
